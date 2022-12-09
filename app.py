39f8de3910e17f124da07845e5bc91f8da62cec3
imngooh <goodjw0125@gmail.com>
2022-12-09 22:30:08 +0900

    1차 테스트

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from PIL import Image
+
+st.title('마스크 착용 감지 모델')
+
+
+# requests.get('https://github.com/imngooh/mini_streamlit/raw/main/acc_0.943_vgg19.h5')
+
+
+model = tf.keras.models.load_model('acc_0.943_vgg19.h5')
+model.summary()
+tf.keras.utils.plot_model(model)
